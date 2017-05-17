0f6a810ff977204d8b80ceca13f7f9395a83dddd
Andrew Lampinen <lampinen@stanford.edu>
2017-05-17 14:52:17 -0700

    maintaining question order

--- questions/csv2json.py
+++ questions/csv2json.py
@@ -1,14 +1,15 @@
 import csv
 import json
+from collections import OrderedDict
 
 length = 33 # hard coded, total number of columns 
 data = {}
 with open('answers2017.csv','r') as f:
 	reader = csv.reader(f)
 	questions = reader.next()[1:]
-	data = dict([(q,{}) for q in questions])
+	data = OrderedDict([(q,{}) for q in questions])
 	for row in reader:
-		for i in range(1,length):
+		for i in range(1,length+1):
 			cur = data[questions[i-1]]
 			item = row[i]
 			if item == "":
